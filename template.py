99873e67c9b4e6c7730754e40dd578cff10e71ad
Nils Streedain <streedan@oregonstate.edu>
2023-04-19 02:40:14 +0000

    Code cleanup/optimization

--- template.py
+++ template.py
@@ -22,6 +22,7 @@ print(p.readline().strip())
 # get ciphertext
 ciphertext = p.readline().strip()
 print('ciphertext: %s' % ciphertext)
+
 # get newline
 print(p.readline().strip())
 
@@ -41,34 +42,25 @@ print(p.readline().strip())
 # get the secret number
 secret_number = p.readline().strip()
 print('secret_number: %s' % secret_number)
+
 # get the byte of the secret number
 byte_secret_number = secret_number.decode('hex')
 
-plain_secret_array = []
+secret_array = []
 
 # write your code to get the plaintext of the secret numbers and on
+def xor_decrypt(data, key):
+	return [chr(ord(c1) ^ ord(c2)) for c1, c2 in zip(data, key)]
+
 # Extract the key by XORing plaintext with ciphertext
-for (text_c, key_c) in zip(byte_plaintext, byte_ciphertext):
-	key.append(chr(ord(text_c) ^ ord(key_c)))
-
-# Decrypt the secret number using the extracted key
-for (text_c, key_c) in zip(byte_secret_number, key):
-	plain_secret_array.append(chr(ord(text_c) ^ ord(key_c)))
+key = xor_decrypt(byte_plaintext, byte_ciphertext)
 
 print(p.readline().strip())
-p.sendline(''.join(plain_secret_array).encode('hex'))
 
-print(p.readline().strip())
-print(p.readline().strip())
-print(p.readline().strip())
-print(p.readline().strip())
-print(p.readline().strip())
+# Decrypt the secret number using the extracted key and send to process
+p.sendline(''.join(xor_decrypt(byte_secret_number, key)).encode('hex'))
 
-bytes_flag = p.readline().strip().decode('hex')
+for _ in range(5): print(p.readline().strip())
 
-plain_flag = []
-
-for (text_c, key_c) in zip(bytes_flag, key):
-        plain_flag.append(chr(ord(text_c) ^ ord(key_c)))
-
-print(''.join(plain_flag))
+# Decrypt/print flag
+print(''.join(xor_decrypt(p.readline().strip().decode('hex'), key)))
